436f731a6704067a3a955888ff4464a222bdab08
mrlonely <mrlonely1226@163.com>
2025-03-29 01:31:03 +0800

    feat: add python配置

---
+++ python/ds-demo.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+client = OpenAI(api_key="sk-efb36982f47d490891c5942979085543", base_url="https://api.deepseek.com")
+
+# Round 1
+messages = [{"role": "user", "content": "9.11 and 9.8, which is greater?"}]
+response = client.chat.completions.create(
+    model="deepseek-reasoner",
+    messages=messages
+)
+print(response)
+reasoning_content = response.choices[0].message.reasoning_content
+content = response.choices[0].message.content
+
+# Round 2
+messages.append({'role': 'assistant', 'content': content})
+messages.append({'role': 'user', 'content': "How many Rs are there in the word 'strawberry'?"})
+response = client.chat.completions.create(
+    model="deepseek-reasoner",
+    messages=messages
+)
